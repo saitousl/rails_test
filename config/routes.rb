8272a868f6fbd538722c9616fef4ee01a7f2b4ac
saitousl <saito@usl.cs.tut.ac.jp>
2017-07-18 21:16:57 +0000

    sample

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :topics
+  get 'hello/index'
+  #get    'topics/:id' => 'topics#show'
+  #get    'topics/new' => 'topics#new'
+  #post   'topics'     => 'topics#create'
+  #get    'topics/:id/edit' => 'topics#edit'
+  #delete 'topics/:id' => 'topics#destroy'
+  #put    'topics/:id' => 'topics#update'
+  root :to => 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
+@topic = Topic.new
+@topic.title = 'seed'
+
 # This file should contain all the record creation needed to seed the database with its default values.
+#
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
